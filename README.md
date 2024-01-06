10ca97f5dd8370d459bbf3fada0926b24766b47d
getarrenspieler <getarrenspieler@gmail.com>
2024-01-06 15:23:59 +0300

    spring boot project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # highload
+
+JDK 17
+https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip
---
+++ src/test/java/ru/gts/highload/HighloadApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gts.highload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
